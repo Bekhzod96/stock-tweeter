e4dd3f3adc3751949e316f8dd6f47e14a36ceb56
Bekhzod96 <mr.begzodjon96@mail.ru>
2020-08-04 20:49:00 +0300

    Deployment credentials changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,12 +39,12 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://stock-tweeter.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://stock-tweeter.herokuapp.com/', /https:\/\/stock-tweeter.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -88,7 +88,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
